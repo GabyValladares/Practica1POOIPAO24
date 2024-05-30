856d030228ceddc950bbe7a830277277fb3a436a
GabyValladares <126087266+GabyValladares@users.noreply.github.com>
2024-05-30 09:42:45 -0500

    Main con programación estructura de datos y usamos una lista

---
+++ src/vista/Main.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Main {
+    //1. MÉTODO TRADUCTOR O INTERPRETE
+    public static void main(String[] args) {
+        System.out.println("HOLA");
+        //DECLARACIÓN DE LISTA
+        String[] listaDatos=new String[5];
+        //VARIABLES
+        System.out.println("DATOS DEL USUARIO:");
+        String nombre="Gaby";
+        String apellido="Valladares";
+        char inicial='G';
+        String direccion="Ibarra";
+        String correoElectronico="gvalladares@ist17dejulio.edu.ec";
+        listaDatos[0]=nombre;
+        listaDatos[1]=apellido;
+        listaDatos[2]=direccion;
+        listaDatos[3]=correoElectronico;
+        
+        System.out.println("1)"+listaDatos[0]);
+        System.out.println("2)"+listaDatos[1]);
+        System.out.println("3)"+listaDatos[2]);
+        System.out.println("4)"+listaDatos[3]);
+        System.out.println("5)"+listaDatos[4]);
+        
+        
+        System.out.println("DATOS DEL USUARIO:");
+        String nombre1="Geovana";
+        String apellido1="Valladares";
+        char inicial1='G';
+        String direccion1="Quito";
+        String correoElectronico1="g1valladares@ist17dejulio.edu.ec";
+    
+    }
+}
